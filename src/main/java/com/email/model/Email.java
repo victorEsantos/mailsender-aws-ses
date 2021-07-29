167c5fc48398acf17d20a5b23687cf31f7ca8156
victor santos <victor.santos@neomind.com.br>
2021-07-29 13:02:35 -0300

    Init project and email model

---
+++ src/main/java/com/email/model/Email.java
@@ -0,0 +1,27 @@
+package com.email.model;
+
+import com.email.enums.StatusEmail;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class Email implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
